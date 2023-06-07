3e2132288ca500a239c996ef66b8e57ffd8725fa
Mike-Harwood <92137315+Mike-Harwood@users.noreply.github.com>
2023-06-07 22:03:37 +1200

    Documentation & Video

--- Models/Cast.cs
+++ Models/Cast.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+
         public string? ScreenName { get; set; }
 
         public Guid? MovieId { get; set; }
